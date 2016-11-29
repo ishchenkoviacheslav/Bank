5e1d0959a2ef3d64628d14d6d8a4f27fed251ff2
user <user@DESKTOP-05J4HLG>
2016-11-29 20:51:43 +0200

    новый контроллер

---
+++ Bank/Controllers/AccountFunctionalController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bank.Controllers
+{
+    public class AccountFunctionalController : Controller
+    {
+        // GET: AccountFunctional
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string GetCash()
+        {
+            return "Get Cash";
+        }
+    }
+}
